a04e50ca14f76c84fbeb36ca18ab7e8b8eb9ec53
Sarah Benecke <sbe@amagno.de>
2024-11-14 09:20:31 +0100

    Initial setup

---
+++ GemBoxPoc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using GemBox.Document;
+
+ComponentInfo.SetLicense("LICENSE KEY");
+
+var pages = DocumentModel.Load("crash.html").GetPaginator().Pages;
+
+for (var index = 0; index < pages.Count; index++)
+{
+    var fileName = $"Page-{index}.bmp";
+            
+    var page = pages[index];
+            
+    using var stream = new FileStream(Path.Combine(@"..\..\..\OutputFiles", fileName), FileMode.Create);
+            
+    page.Save(stream, new ImageSaveOptions(ImageSaveFormat.Bmp));
+}
